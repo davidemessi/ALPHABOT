027e54e30acde3fea9fb54974b26f5547b55183c
davidemessi <davide.messi@itiscuneo.eu>
2025-11-06 10:14:55 +0100

    modifiche del 30/11

--- server_TCP.py
+++ server_TCP.py
@@ -1,10 +1,13 @@
 import socket
 from AlphaBot import AlphaBot
 import time
+import sqlite3
 
-ADDRESS = ('0.0.0.0', 6969) #0.0.0.0 indirizzo ip speciale = this host(questo host)
+ADDRESS = ('0.0.0.0', 8080) #0.0.0.0 indirizzo ip speciale = this host(questo host)
 MAX_CONNECTIONS = 3
 BUFFER = 4096
+
+comandoPrecedente = 'stop'
 
 ab = AlphaBot()
 ab.stop() #OBBLIGATORIO
@@ -19,32 +22,60 @@ connection, sender_address = s.accept()
 
 #metto in ascolto il server
 message_bin = connection.recv(BUFFER)
-#print(f"ho ricevuto {message_bin.decode()}")
 
-# if(message_bin.decode()=="avanti"):
-#     #print("Vado avanti")
-#     alphabot.forward()
-#     time.sleep(5)
-#     alphabot.stop()
 
-#USARE UNA VARIABILE AUSIILARIA PER TENER CONTO DELL'ULTIMO COMANDO PREMUTO E SE È UGUALE A
-#QUELLO PREMUTO ORA LO IGNORO IN MODO TALE DA NON MANDARE TROPPI MESSAGGI UGUALI
 while True:
-    print(message_bin.decode())
-    if(message_bin.decode()=="avanti"):
+    message_bin = connection.recv(BUFFER)
+    comando = message_bin.decode().strip()
+    print(f"comandoPrecedente = {comandoPrecedente}")
+    print(f"messaggio ricevuto da client = {comando}")
+
+    # se il comando è uguale al precedente, lo ignoro
+    if comando == comandoPrecedente:
+        continue
+
+    # aggiorno comando precedente
+    comandoPrecedente = comando
+
+    if comando == "avanti":
         ab.forward()
-        message_bin = connection.recv(BUFFER)
-    elif(message_bin.decode()=="indietro"):
+    elif comando == "indietro":
         ab.backward()
-        message_bin = connection.recv(BUFFER)
-    elif(message_bin.decode()=="destra"):
+    elif comando == "destra":
         ab.right()
-        message_bin = connection.recv(BUFFER)
-    elif(message_bin.decode()=="sinistra"):
+    elif comando == "sinistra":
         ab.left()
-        message_bin = connection.recv(BUFFER)
-    elif(message_bin.decode()=="stop"):
+    elif comando == "stop":
         ab.stop()
-        message_bin = connection.recv(BUFFER)
+    elif comando == "quadrato":
+        con = sqlite3.connect("./alphaBot_DB.db")
+        cur = con.cursor()
+        res = cur.execute(f"SELECT movimento FROM Comandi")
+        movimenti = res.fetchall()
+        #print(f"movimenti: {movimenti}")
+
+        for riga in movimenti:
+            movimento = riga[0]
+            print(f"Eseguo: {movimento}")
+
+            if movimento == "avanti":
+                ab.forward()
+            elif movimento == "indietro":
+                ab.backward()
+            elif movimento == "sinistra":
+                ab.left()
+            elif movimento == "destra":
+                ab.right()
+            elif movimento == "aspetta":
+                time.sleep(1)
+
+        ab.stop()
+        con.close()
+
+        #DA PROVARE SE FUNZIONA QUESTO CODICE CON IL DB
+
+        
+
+        
 
 s.close()
